1ac45a8575e47c092c931c9e746a43b391d06138
superido <superk123@163.com>
2018-10-11 16:36:03 +0800

    cmservice,opentsdb

--- snmp-nms/src/test/java/cn/cloudwiz/dalian/snmp/nms/TestCMService.java
+++ snmp-nms/src/test/java/cn/cloudwiz/dalian/snmp/nms/TestCMService.java
@@ -16,22 +16,20 @@ import java.util.Properties;
 
 public class TestCMService {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    public static void main(String[] args) throws IOException, SQLException, InterruptedException {
 //        InputStream is = TestCMService.class.getResourceAsStream("cloudmon.conf");
 //        properties.load(is);
         // 设置属性文件
         final Properties properties = new Properties();
         // cmservice
-        properties.setProperty("cmdb_service", "http://123.206.91.96:8080/_cmdb");
+        properties.setProperty("cmdb_service", "http://123.206.91.96:8080/_cmdb");//cmservice服务地址
         // opentsdb
-        properties.setProperty("metrics_server_url", "http://123.206.91.96:8080/_tsdb");
+        properties.setProperty("metrics_server_url", "http://123.206.91.96:8080/_tsdb");//tsdb服务地址
         byte[] encodeBase64 = Base64.encodeBase64("Cloud".getBytes("UTF-8"));
         String s = new String(encodeBase64);
-        properties.setProperty("mysql_host", "123.206.91.96");
-        properties.setProperty("mysql_port", "3308");
         properties.setProperty("mysql_username", "CloudInsight");
-        properties.setProperty("mysql_password", new String(encodeBase64));
-        properties.setProperty("mysql_url", "jdbc:mysql://123.206.91.96:3308/grafana");
+        properties.setProperty("mysql_password", new String(encodeBase64));// 密码需要base64加密
+        properties.setProperty("mysql_url", "jdbc:mysql://123.206.91.96:3308/grafana");// OrgTokenMap.java中获取token需要检索的数据库。
 
         // 初始化
         HttpClient client = HttpClient.getInstance();
@@ -39,13 +37,17 @@ public class TestCMService {
         CMDBProxy.initialize(properties);
         TestCMService test = new TestCMService();
         // 获取CI数据
-        test.getCIByKey(1, 1, CIType.Device, "172.21.16.14_VM1614centos_device_ram0");
+//        test.getCIByKey(1, 1, CIType.Device, "172.21.16.14_VM1614centos_device_ram0");
         // 创建CI数据
 //        test.createCI(9, 1, CIType.Device, "172.21.16.14_VM1614centos_device_ram0");
 
         // 写OpenTSDb
-//        OpentsdbProxy.initInstance(properties);
-//        test.writeOpenTsDB(1, 1);
+        System.out.println("begin tsdb init");
+        OpentsdbProxy.initInstance(properties);
+        // 因为initInstance方法中异步检索token信息，如果不sleep会导致写数据的时候缺少token而失败。实际工程中可在工程启动时init
+        Thread.sleep(5000);
+        System.out.println("writeOpenTsDB");
+        test.writeOpenTsDB(1, 1);
     }
 
     public void getCIByKey(long orgId, long sysId, CIType type, String key) {
@@ -61,7 +63,7 @@ public class TestCMService {
         ci.setType(type);
         ci.addAttribute("devicename", "HP");
         ci.addAttribute("ip", "192.196.1.2");
-        ci.addAttribute("deviceType", "核心路由器");
+        ci.addAttribute("deviceType", "Core Router");
         HttpResult result = CMDBProxy.createCI(orgId, sysId, ci);
         System.out.print(result.toString());
     }
@@ -70,16 +72,16 @@ public class TestCMService {
 
         Map<String, String> tags = new HashMap<>();
         tags.put("org", Long.toString(orgId));
-        tags.put("name", "abc");
+        tags.put("host", "192.168.1.11"); // host项目必须有，使用机器IP
+        tags.put("deviceId", "1");// 比如获取风扇转速数据的时候，一台机器有多个风扇，需要一个tag来区别不同风扇。
         OpentsdbProxy.getInstance().writeOneDataPoint(
                 orgId,
                 sysId,
-                "net.if.out.errors",
+                "net.if.out.errors",// 该字段为自定义监控项中设置的指标字段
                 System.currentTimeMillis()/1000,
-                9999,
+                8888,
                 tags
         );
-
     }
 }
 
